8627839b77d93f37368187e34a40c2f7f6bc954c
AGOODFROG <caden.j.quirion@gmail.com>
2024-03-22 17:35:08 -0400

    added logic

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("test")
+let num1
+let num2
+let oporator
+
+function add(num1 , num2){
+    return num1 + num2
+}
+function subtract(num1, num2){
+    return num1 - num2
+}
+function mutplie(num1, num2){
+    return num1 * num2
+}
+function divide(num1, num2){
+    return num1/num2
+}
+function comput(num1, oporator, num2){
+    switch(oporator){
+        case("+"):{
+            return add(num1,num2)
+        }
+        case("-"):{
+            return subtract(num1,num2)
+        }
+        case("*"):{
+            return mutplie(num1,num2)
+        }
+        case("/"):{
+            return(num1,num2)
+        }
+
+    }
+}
+
+console.log(comput(4,"/",2))
